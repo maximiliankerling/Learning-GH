e682d03666bfd9a5566dc2e4df231bafce8ae8cc
Maximilian Kerling <maximilian.kerling@gmail.com>
2021-04-24 12:58:35 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Maximilian Kerling on 24.04.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
